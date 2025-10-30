f93c753fbb72b08e908d482f4c9e91c91d547aaf
Ali Jalilpour <contact.jalilpour@gmail.com>
2025-10-30 14:12:04 +0330

    Update Header.jsx

--- alowishus/src/components/Header.jsx
+++ alowishus/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                     <img src="/image/logo.png" alt="logo" />
                 </a>
                 {/* navigation */}
-                <div className="flex items-center md:gap-x-5 lg:gap-x-10">
+                <div className="flex items-center gap-x-10">
                     <nav className="hidden md:flex items-center md:gap-x-5 lg:gap-x-10 font-medium lg:text-base text-eerie-black *:relative *:after:content-[''] *:after:absolute *:after:left-0 *:after:-bottom-2 *:after:w-0 *:after:h-0.5 *:after:bg-eerie-black *:after:transition-all *:after:duration-300 *:after:delay-75 *:hover:after:w-full">
                         <a href="#">Cafe Munu</a>
                         <a href="#">About Us</a>
@@ -16,7 +16,7 @@ const Header = () => {
                         <a href="#">Alowishus Catering</a>
                     </nav>
                     {/* shopping box & gift*/}
-                    <div className="flex items-center gap-x-5">
+                    <div className="flex items-center gap-x-2.5 lg:gap-x-5">
                         {/* shopping box */}
                         <a href="#" className="rounded-full flex items-center justify-center max-sm:p-2 sm:p-3 xl:p-4 bg-white hover:text-white hover:bg-eerie-black transition-all duration-300 delay-75 shadow-2xl">
                             <svg className="size-6" viewBox="0 0 24 24" fill="currentColor">
@@ -24,9 +24,16 @@ const Header = () => {
                             </svg>
                         </a>
                         {/* gift */}
-                        <a href="#"className="bg-eerie-black hidden items-center justify-center md:flex lg:py-3 xl:py-3.75 px-7.5 text-white lg:text-sm xl:text-base/6.5 font-medium rounded-full hover:bg-white hover:text-eerie-black transition-all duration-300 delay-75 shadow-2xl">
-                            BUY GIFT VOUCHERS
-                        </a>
+                        <div className="max-md:hidden">
+                            <a href="#" className="rounded-full flex items-center justify-center max-sm:p-2 sm:p-3 lg:hidden bg-eerie-black hover:text-eerie-black hover:bg-white text-white transition-all duration-300 delay-75 shadow-2xl">
+                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                                </svg>
+                            </a>
+                            <a href="#"className="bg-eerie-black hidden items-center justify-center lg:flex lg:py-3 xl:py-3.75 px-7.5 text-white lg:text-sm xl:text-base/6.5 font-medium rounded-full hover:bg-white hover:text-eerie-black transition-all duration-300 delay-75 shadow-2xl">
+                                BUY GIFT VOUCHERS
+                            </a>
+                        </div>
                         {/* bars */}
                         <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="max-sm:size-8 sm:size-10 md:hidden">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
