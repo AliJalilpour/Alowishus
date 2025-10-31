bc1105bb07e2e8abf73c09f5d899dfe59e613460
Ali Jalilpour <contact.jalilpour@gmail.com>
2025-10-31 20:50:21 +0330

    update  files

--- alowishus/src/components/Header/Header.jsx
+++ alowishus/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import MobileMenu from "./MobileMunu";
 const Header = () => {
     const [open, setOpen] = useState(false);
     return (
-        <header className="container mx-auto px-5 mt-6">
+        <header className="container mx-auto px-5 pt-6">
             {/* wrapper */}
             <div className="flex items-center justify-between">
                 {/* logo */}
@@ -12,17 +12,17 @@ const Header = () => {
                     <img src="/image/logo.png" alt="logo" />
                 </a>
                 {/* navigation */}
-                <div className="flex items-center gap-x-10">
-                    <nav className="hidden md:flex items-center md:gap-x-5 lg:gap-x-10 font-medium lg:text-base text-eerie-black *:relative *:after:content-[''] *:after:absolute *:after:left-0 *:after:-bottom-2 *:after:w-0 *:after:h-0.5 *:after:bg-eerie-black *:after:transition-all *:after:duration-300 *:after:delay-75 *:hover:after:w-full">
+                <div className="flex items-center justify-between gap-x-10">
+                    <nav className="hidden md:flex items-center md:gap-x-5 lg:gap-x-10 font-jost font-medium lg:text-base text-eerie-black *:relative *:after:content-[''] *:after:absolute *:after:left-0 *:after:-bottom-0.5 *:after:w-0 *:after:h-0.5 *:after:bg-eerie-black *:after:transition-all *:after:duration-300 *:after:delay-75 *:hover:after:w-full">
                         <a href="#">Cafe Munu</a>
                         <a href="#">About Us</a>
                         <a href="#">Find Us</a>
                         <a href="#">Alowishus Catering</a>
                     </nav>
                     {/* shopping box & gift*/}
-                    <div className="flex items-center gap-x-2.5 lg:gap-x-5">
+                    <div className="flex items-center justify-between">
                         {/* shopping box */}
-                        <a href="#" className="rounded-full flex items-center justify-center max-sm:p-2 sm:p-3 xl:p-4 bg-white hover:text-white hover:bg-eerie-black transition-all duration-300 delay-75 shadow-2xl z-0">
+                        <a href="#" className="rounded-full flex items-center justify-center max-sm:p-2 mr-2.5 lg:mr-5 sm:p-3 xl:p-4 bg-white hover:text-white hover:bg-eerie-black transition-all duration-300 delay-75 shadow-2xl z-0">
                             <svg className="size-6" viewBox="0 0 24 24" fill="currentColor">
                                 <path d="M8.13201 2.50391L4.42001 8.99991H3.00001C2.68801 8.99991 2.39501 9.14591 2.20501 9.39291C2.01601 9.64091 1.95301 9.96291 2.03501 10.2629L4.83401 20.5259C5.07001 21.3939 5.86401 21.9999 6.76401 21.9999H17.237C18.135 21.9999 18.929 21.3949 19.167 20.5249L21.966 10.2619C22.048 9.96191 21.986 9.63991 21.796 9.39191C21.605 9.14591 21.312 8.99991 21 8.99991H19.58L15.868 2.50391L14.132 3.49591L17.277 8.99991H6.72301L9.86801 3.49591L8.13201 2.50391ZM14 12.9999H16V17.9999H14V12.9999ZM8.00001 12.9999H10V17.9999H8.00001V12.9999Z"/>
                             </svg>
@@ -34,7 +34,7 @@ const Header = () => {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                                 </svg>
                             </a>
-                            <a href="#"className="bg-eerie-black hidden items-center justify-center lg:flex lg:py-3 xl:py-3.75 px-7.5 text-white lg:text-sm xl:text-base/6.5 font-medium rounded-full hover:bg-white hover:text-eerie-black transition-all duration-300 delay-75 shadow-2xl">
+                            <a href="#"className="bg-eerie-black hidden items-center justify-center lg:flex lg:py-3 xl:py-3.75 px-7.5 font-jost text-white lg:text-sm xl:text-base/6.5 font-medium rounded-full hover:bg-white hover:text-eerie-black transition-all duration-300 delay-75 shadow-2xl">
                                 BUY GIFT VOUCHERS
                             </a>
                         </div>
--- alowishus/src/components/Main/Introduction.jsx
+++ alowishus/src/components/Main/Introduction.jsx
@@ -2,20 +2,20 @@ const Introduction = () => {
     return (
         <section>
             {/* wrapper */}
-            <div className="flex items-center justify-between pb-40">
+            <div className="flex flex-col-reverse md:flex-row items-center justify-between pb-40">
                 {/* introduction */}
                 <div className="flex-1">
                     <div>
-                        <span className="font-jost font-extrabold text-9xl text-eerie-black">Alowishus Delicious Coffee</span>
-                        <div className="inline-flex ml-5">
+                        <span className="font-jost font-extrabold sm:text-9xl md:text-6xl lg:text-8xl xl:text-9xl text-eerie-black">Alowishus Delicious Coffee</span>
+                        <div className="inline-flex ml-5 md:w-10 lg:w-15 xl:w-20">
                             <img src="/image/image 3.png" alt="cafe" />
                         </div>
-                        <p className="font-jost text-2xl text-granite-gray mt-7.5 max-w-132.5">A drink from the ‘My Alowishus’ bottled brews range OR grab one of our delicious coffee’s.</p>
+                        <p className="font-jost text-xl xl:text-2xl text-granite-gray md:mt-5 lg:mt-7.5 max-w-100 xl:max-w-132.5">A drink from the ‘My Alowishus’ bottled brews range OR grab one of our delicious coffee’s.</p>
                     </div>
                     {/* buttons */}
-                    <div className="font-jost font-medium text-lg flex items-center gap-x-7.5 mt-15">
-                        <a href="#" className="flex items-center justify-center py-5 px-10 bg-eerie-black text-white rounded-full hover:bg-white hover:text-eerie-black transition-all duration-300 delay-75">DOWNLOAD APP</a>
-                        <a href="#" className="text-eerie-black relative after:content-[''] after:absolute after:left-0 after:right-0 after:-bottom-0.5 after:h-0.5 after:w-full after:bg-eerie-black">SHOP COFFEE</a>
+                    <div className="font-jost font-medium md:text-base lg:text-base xl:text-lg flex items-center gap-x-7.5 md:mt-5 lg:mt-7.5 xl:mt-15">
+                        <a href="#" className="flex items-center justify-center md:py-4 xl:py-5 px-10 bg-eerie-black text-white rounded-full hover:bg-white hover:text-eerie-black transition-all duration-300 delay-75">DOWNLOAD APP</a>
+                        <a href="#" className="text-eerie-black relative after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:h-0.5 after:w-0 after:bg-eerie-black after:transition-all after:duration-300 after:delay-75 hover:after:w-full">SHOP COFFEE</a>
                     </div>
                 </div>
                 {/* image */}
